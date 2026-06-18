9be68fc600c0959fd8ba843b5b4daf9770ef5b70
MrZidez <htoa94042@gmail.com>
2026-06-18 16:26:30 +0700

    Update vpn_checker.py

--- vpn_checker.py
+++ vpn_checker.py
@@ -6,8 +6,14 @@ print("🚀 START")
 
 # Просто список URL
 urls = [
-    "https://raw.githubusercontent.com/nikita29a/FreeProxyList/main/mirror/1.txt",
-    "https://raw.githubusercontent.com/nikita29a/FreeProxyList/main/mirror/2.txt",
+    "https://raw.githubusercontent.com/dmitriistekolnikov/Free_vpns_for_Russ/refs/heads/main/Vip.txt",
+    "https://raw.githubusercontent.com/dmitriistekolnikov/Free_vpns_for_Russ/refs/heads/main/YouTube.txt",
+    "https://gitlab.com/zieng2/wl/raw/main/vless_universal.txt",
+    "https://storage.yandexcloud.net/mystorage123/whitelist.txt",
+    "https://gist.githubusercontent.com/HalyavusVPNUS/a93def732d3c624029c09c393dd0772e/raw/e310946a53d9cd7910bb4381e7fceab83e1f8462/%25D0%25BA%25D0%25BE%25D0%25BD%25D1%2584%25D0%25B8%25D0%25B3%25D0%25B8",
+    "https://gist.githubusercontent.com/HalyavusVPNUS/a93def732d3c624029c09c393dd0772e/raw/0f61abfd23bfc411b14385a95cfdb90498514a45/%25D0%25BA%25D0%25BE%25D0%25BD%25D1%2584%25D0%25B8%25D0%25B3%25D0%25B8",
+    "https://gist.githubusercontent.com/HalyavusVPNUS/a93def732d3c624029c09c393dd0772e/raw/07febf8476e2603723f94a777542ab69a5c7fa2f/%25D0%25BA%25D0%25BE%25D0%25BD%25D1%2584%25D0%25B8%25D0%25B3%25D0%25B8",
+    "https://gist.githubusercontent.com/j80547013-max/1ed9f2d72fd7613eda3c4a36c96955cb/raw/bfd36277ccf212a8ed2800708a749efbcd5a0885/gistfile1.txt",
 ]
 
 all_keys = []
